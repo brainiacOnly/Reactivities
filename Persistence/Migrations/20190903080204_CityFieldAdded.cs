73cafcea9596deff366a3f59d4a2d5e91d77d9e2
Maksym Berezhnyak <maxonly9@gmail.com>
2019-09-03 13:42:34 +0300

    End Section 4

---
+++ Persistence/Migrations/20190903080204_CityFieldAdded.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class CityFieldAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Activities",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Activities");
+        }
+    }
+}
